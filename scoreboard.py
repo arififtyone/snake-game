50f98bb6bc83f9c98e38e497a1e617a42e61a361
arififtyone <arianaguilak@Arianas-Air.lan>
2023-12-21 09:21:00 -0800

    add wall collision detection

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 20, "normal")
 
 
 class Scoreboard(Turtle):
@@ -9,9 +11,14 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.color("white")
         self.goto(0, 270)
-        self.write(f"Score: {self.score}", move=False, align="center", font=("Courier", 20, "normal"))
+        self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
 
 
     def update_score(self):
         self.clear()
-        self.write(f"Score: {self.score}", move=False, align="center", font=("Courier", 20, "normal"))
+        self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+
+    
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", move=False, align="center", font=FONT)
